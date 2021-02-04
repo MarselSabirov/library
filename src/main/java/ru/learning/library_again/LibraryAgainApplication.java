ae74537911ac4aed556fe7d0fd2b28ead77309d9
marsel <marssabi@yandex.ru>
2021-02-04 03:37:36 -0800

    Initial commit

---
+++ src/main/java/ru/learning/library_again/LibraryAgainApplication.java
@@ -0,0 +1,13 @@
+package ru.learning.library_again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAgainApplication.class, args);
+    }
+
+}
